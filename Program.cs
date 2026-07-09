1945251288cc5860271ff0e63341f5a3c6762472
Bui Quang Hop <dtc235210027@ictu.edu.vn>
2026-07-09 17:03:33 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int sum = 0;
+        for (int i = 2; i <= 40; i += 2)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"Tong so chan tu 2 den 40 la: {sum}");
+    }
+}
